ae9a55816c1733ce7ab41f6a36ad4e0ae52efdfe
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2023-10-25 18:35:05 +0500

    Build Header and Grid

--- src/Root/index.jsx
+++ src/Root/index.jsx
@@ -1,11 +1,27 @@
-
+import Header from "../components/Header";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const Root = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <Header />
+      </GridItem>
+      <Show above="md">
+        <GridItem bg="orange" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green" area="main">
+        Main
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default Root
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import Root from "./Root";
 import "./index.css";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Root />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Root />
+    </ChakraProvider>
   </React.StrictMode>
 );
